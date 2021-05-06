60e0c7199dc815b403e15879df3b650764537432
phatdroid94@gmail.com <phatdroid94@gmail.com>
2021-05-06 20:41:13 +0700

    update

--- app/src/main/java/com/example/stringresource23032021/MainActivity.java
+++ app/src/main/java/com/example/stringresource23032021/MainActivity.java
@@ -3,12 +3,39 @@ package com.example.stringresource23032021;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.ToggleButton;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText mEdtUser,mEdtPassword;
+    Button mBtnLogin;
+    TextView mTvInfo;
+    ToggleButton mToggleLan;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mEdtPassword = findViewById(R.id.editTextPassWord);
+        mEdtUser = findViewById(R.id.editTextUser);
+        mBtnLogin = findViewById(R.id.buttonLogin);
+        mTvInfo = findViewById(R.id.textViewInfo);
+        mToggleLan = findViewById(R.id.toggleLanguage);
+
+        mBtnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String username = mEdtUser.getText().toString();
+                String password = mEdtPassword.getText().toString();
+
+
+            }
+        });
+
+
     }
 }
