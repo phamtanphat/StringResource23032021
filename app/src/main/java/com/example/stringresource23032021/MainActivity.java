41582724a84f68dc92e9aa3703eb96167888a454
phatdroid94@gmail.com <phatdroid94@gmail.com>
2021-05-06 20:57:28 +0700

    method change language

--- app/src/main/java/com/example/stringresource23032021/MainActivity.java
+++ app/src/main/java/com/example/stringresource23032021/MainActivity.java
@@ -2,12 +2,17 @@ package com.example.stringresource23032021;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.res.Configuration;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.ToggleButton;
+
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,6 +23,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setLocale("en");
         setContentView(R.layout.activity_main);
 
         mEdtPassword = findViewById(R.id.editTextPassWord);
@@ -38,7 +44,12 @@ public class MainActivity extends AppCompatActivity {
                 mTvInfo.setText(textKeyUser + " : " + username + "\n" + textKeyPassword + " : " + password);
             }
         });
-
-
+    }
+    private void setLocale(String languageCode) {
+        Locale locale = new Locale(languageCode);
+        Locale.setDefault(locale);
+        Configuration config = getResources().getConfiguration();
+        config.setLocale(locale);
+        getResources().updateConfiguration(config, getResources().getDisplayMetrics());
     }
 }
