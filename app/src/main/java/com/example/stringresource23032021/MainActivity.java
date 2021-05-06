ea32c05e592043afb97fc0652bb12166de37cd1b
phatdroid94@gmail.com <phatdroid94@gmail.com>
2021-05-06 21:25:34 +0700

    finish

--- app/src/main/java/com/example/stringresource23032021/MainActivity.java
+++ app/src/main/java/com/example/stringresource23032021/MainActivity.java
@@ -3,11 +3,14 @@ package com.example.stringresource23032021;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.res.Configuration;
 import android.content.res.Resources;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.ToggleButton;
@@ -20,10 +23,13 @@ public class MainActivity extends AppCompatActivity {
     Button mBtnLogin;
     TextView mTvInfo;
     ToggleButton mToggleLan;
+    SharedPreferences sharedPreferences;
+    SharedPreferences.Editor editor;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setLocale("en");
+        sharedPreferences = getSharedPreferences("AppCache",MODE_PRIVATE);
+        setLocale(sharedPreferences.getString("lang",""));
         setContentView(R.layout.activity_main);
 
         mEdtPassword = findViewById(R.id.editTextPassWord);
@@ -31,6 +37,8 @@ public class MainActivity extends AppCompatActivity {
         mBtnLogin = findViewById(R.id.buttonLogin);
         mTvInfo = findViewById(R.id.textViewInfo);
         mToggleLan = findViewById(R.id.toggleLanguage);
+
+        mToggleLan.setChecked(sharedPreferences.getBoolean("checked",false));
 
         mBtnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -42,6 +50,25 @@ public class MainActivity extends AppCompatActivity {
                 String textKeyPassword = getResources().getString(R.string.text_password);
 
                 mTvInfo.setText(textKeyUser + " : " + username + "\n" + textKeyPassword + " : " + password);
+            }
+        });
+        mToggleLan.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                editor = sharedPreferences.edit();
+                if (b){
+                    // language vn
+                    // no sql : dùng key và value để lưu trữ
+                    editor.putString("lang","vi");
+                    editor.putBoolean("checked",true);
+                }else{
+                    editor.putString("lang","en");
+                    editor.putBoolean("checked",false);
+                }
+                editor.apply();
+                Intent intent = new Intent(MainActivity.this,MainActivity.class);
+                startActivity(intent);
+                finish();
             }
         });
     }
